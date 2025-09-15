1672d15842cfa25869e0805c7468b188b90456ed
Fenton Haslam <fentonhaslam@gmail.com>
2025-09-15 20:54:47 +0800

    added changes

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-const s3 = new AWS.S3({ region: "us-east-1" });
+const s3 = new AWS.S3({
+  region: "eu-west-1",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 const bucketName = "snareshare"; // ✅ update with your bucket name
 
 // Endpoint: get presigned URLs
